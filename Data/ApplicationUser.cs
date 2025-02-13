83bab92b63e03be98fcea218fc09857e111921c0
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-13 12:15:18 +0100

    add ApplicationUser

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TyskaForSmaUpptackare.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public bool isBlocked { get; set; }
+        public DateTime? BlockedAt { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        public ICollection<Subscription> Subscriptions { get; set; }
+    }
+}
