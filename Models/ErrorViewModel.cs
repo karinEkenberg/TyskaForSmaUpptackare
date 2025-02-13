d373619ca4a1d12e3c35b7eba4fdac613767a776
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-13 11:03:37 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TyskaForSmaUpptackare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
