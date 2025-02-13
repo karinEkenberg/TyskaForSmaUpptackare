71a8f305f680e3e3a94fef9e9c19a388fe007351
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-13 12:22:33 +0100

    add Product.cs

---
+++ Data/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TyskaForSmaUpptackare.Data
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
