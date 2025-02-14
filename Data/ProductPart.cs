1847607a67252f89434a35369ebc4b0280fe56ec
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-14 10:44:53 +0100

    fix warnings in ProductPart

--- Data/ProductPart.cs
+++ Data/ProductPart.cs
@@ -9,12 +9,12 @@ namespace TyskaForSmaUpptackare.Data
         public int PartId { get; set; }
         [Required]
         [MaxLength(50)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int ProductId { get; set; }
         [ForeignKey("ProductId")]
-        public Product Product { get; set; }
-        public string AudioUrl { get; set; }
-        public string ImageUrl { get; set; }
+        public Product Product { get; set; } = default!;
+        public string AudioUrl { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
         public ICollection<ProductItem> Items { get; set; } = new List<ProductItem>();
     }
 }
