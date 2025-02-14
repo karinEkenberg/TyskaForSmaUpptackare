24736ed2e63a7c0ee86ab5db548faae518a9d6e6
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-14 10:45:58 +0100

    fix warnings in ProductItem

--- Data/ProductItem.cs
+++ Data/ProductItem.cs
@@ -9,12 +9,12 @@ namespace TyskaForSmaUpptackare.Data
         public int ItemId { get; set; }
         [Required]
         [MaxLength(50)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int ProductPartId { get; set; }
         [ForeignKey("ProductPartId")]
-        public ProductPart ProductPart { get; set; }
-        public string ImageUrl { get; set; }
-        public string AudioUrl { get; set; }
+        public ProductPart ProductPart { get; set; } = default!;
+        public string ImageUrl { get; set; } = string.Empty;  
+        public string AudioUrl { get; set; } = string.Empty;
         public int? ParentItemId { get; set; }
         public ProductItem ParentItem { get; set; }
         public ICollection<ProductItem> ChildItems { get; set; } = new List<ProductItem>();
