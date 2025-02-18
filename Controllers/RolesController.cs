da665c4f9869101d723ba503fba2fa8f94c92833
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-18 14:08:15 +0100

    add customer role

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-using static System.Console;    
+using System;
+using System.Threading.Tasks;
+using static System.Console;
 
 namespace TyskaForSmaUpptackare.Controllers
 {
     public class RolesController : Controller
     {
-        private string AdminRole = "Administrators";
-        private string UserEmail = "k.ekenberg.dev@gmail.com";
+        // Hårdkodade roller och e-postadresser
+        private readonly string AdminRole = "Administrators";
+        private readonly string UserEmail = "k.ekenberg.dev@gmail.com";
+        private const string CustomerRole = "Customer";
+        private const string CustomerEmail = "karin@exempel.se";
+
         private readonly RoleManager<IdentityRole> roleManager;
         private readonly UserManager<IdentityUser> userManager;
-        
+
         public RolesController(RoleManager<IdentityRole> roleManager,
             UserManager<IdentityUser> userManager)
         {
@@ -20,43 +26,23 @@ namespace TyskaForSmaUpptackare.Controllers
 
         public async Task<IActionResult> Index()
         {
-            if(!(await roleManager.RoleExistsAsync(AdminRole))) 
+            if (!(await roleManager.RoleExistsAsync(AdminRole)))
             {
                 await roleManager.CreateAsync(new IdentityRole(AdminRole));
             }
+
             IdentityUser user = await userManager.FindByEmailAsync(UserEmail);
 
             if (user == null)
             {
-                user = new();
+                user = new IdentityUser();
                 user.UserName = UserEmail;
                 user.Email = UserEmail;
 
-                IdentityResult result = await userManager.CreateAsync(
-                    user, "Pa$$w0rd");
+                IdentityResult result = await userManager.CreateAsync(user, "Pa$$w0rd");
                 if (result.Succeeded)
                 {
-                    WriteLine($"User {user.UserName} created successfully.");
-                }
-                else
-                {
-                    foreach(IdentityError error in result.Errors)
-                    {
-                        WriteLine(error.Description);
-                    }
-                }
-            }
-
-            if(!user.EmailConfirmed)
-            {
-                string token = await userManager
-                   .GenerateEmailConfirmationTokenAsync(user);
-
-                IdentityResult result = await userManager
-                    .ConfirmEmailAsync(user, token);
-                if (result.Succeeded)
-                {
-                    WriteLine($"User {user.UserName} email confirmed sucessfully.");
+                    WriteLine($"Användare {user.UserName} skapades framgångsrikt.");
                 }
                 else
                 {
@@ -66,13 +52,30 @@ namespace TyskaForSmaUpptackare.Controllers
                     }
                 }
             }
+
+            if (!user.EmailConfirmed)
+            {
+                string token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                IdentityResult result = await userManager.ConfirmEmailAsync(user, token);
+                if (result.Succeeded)
+                {
+                    WriteLine($"Användaren {user.UserName} e-post har bekräftats.");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        WriteLine(error.Description);
+                    }
+                }
+            }
+
             if (!(await userManager.IsInRoleAsync(user, AdminRole)))
             {
                 IdentityResult result = await userManager.AddToRoleAsync(user, AdminRole);
-
                 if (result.Succeeded)
                 {
-                    WriteLine($"User {user.UserName} added to {AdminRole} successfully.");
+                    WriteLine($"Användare {user.UserName} tillagd i {AdminRole} framgångsrikt.");
                 }
                 else
                 {
@@ -82,7 +85,51 @@ namespace TyskaForSmaUpptackare.Controllers
                     }
                 }
             }
+
             return Redirect("/");
+        }
+
+        public async Task<IActionResult> AddCustomer()
+        {
+            if (!await roleManager.RoleExistsAsync(CustomerRole))
+            {
+                await roleManager.CreateAsync(new IdentityRole(CustomerRole));
+            }
+
+            var customer = await userManager.FindByEmailAsync(CustomerEmail);
+            if (customer == null)
+            {
+                customer = new IdentityUser
+                {
+                    UserName = CustomerEmail,
+                    Email = CustomerEmail
+                };
+
+                var createResult = await userManager.CreateAsync(customer, "P@$$w0rd");
+                if (!createResult.Succeeded)
+                {
+                    foreach (var error in createResult.Errors)
+                    {
+                        WriteLine(error.Description);
+                    }
+                    return Content("Kunde inte skapa kunden.");
+                }
+            }
+
+            if (!await userManager.IsInRoleAsync(customer, CustomerRole))
+            {
+                var roleResult = await userManager.AddToRoleAsync(customer, CustomerRole);
+                if (!roleResult.Succeeded)
+                {
+                    foreach (var error in roleResult.Errors)
+                    {
+                        WriteLine(error.Description);
+                    }
+                    return Content("Kunde inte lägga till kunden i rollen 'Customer'.");
+                }
+            }
+
+            return Content($"Kunden {CustomerEmail} har lagts till i rollen '{CustomerRole}'.");
         }
     }
 }
