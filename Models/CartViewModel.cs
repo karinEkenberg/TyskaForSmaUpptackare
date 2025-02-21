7f071749ab25da52a99c70f72ca2c7c43b6d19dc
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-21 15:34:30 +0100

    add cart

--- Models/CartViewModel.cs
+++ Models/CartViewModel.cs
@@ -2,5 +2,17 @@
 {
     public class CartViewModel
     {
+        public int CartId { get; set; }
+        public List<CartItemViewModel> Items { get; set; } = new List<CartItemViewModel>();
+
+        public class CartItemViewModel
+        {
+            public int CartItemId { get; set; }
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+            public string ImageUrl { get; set; }
+        }
     }
 }
