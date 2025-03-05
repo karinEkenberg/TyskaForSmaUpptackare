a648bcd281d7c6dc46b98dbdfcf20c5ca715aa74
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-03-05 13:27:42 +0100

    stripe working

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using TyskaForSmaUpptackare.Models;
+using Stripe.Checkout;
+using Microsoft.Extensions.Options;
+
+namespace TyskaForSmaUpptackare.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly StripeSettings _stripeSettings;
+
+        public PaymentController(IOptions<StripeSettings> stripeOptions)
+        {
+            _stripeSettings = stripeOptions.Value;
+            StripeConfiguration.ApiKey = _stripeSettings.SecretKey;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCheckoutSession()
+        {
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string>
+                {
+                    "card",
+                },
+
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = 5000,
+                            Currency = "sek",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "Interaktiv produkt",
+                                Description = "Ett interaktivt element för barn",
+                            },
+                        },
+                        Quantity = 1,
+                    },
+                },
+                Mode = "payment",
+                SuccessUrl = Url.Action("Success", "Payment", null, Request.Scheme),
+                CancelUrl = Url.Action("Cancel", "Payment", null, Request.Scheme),
+            };
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            return Redirect(session.Url);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Cancel()
+        {
+            return View();
+        }
+    }
+}
