bef6092a6335fa7a2cb3ca4904278d39a82a98f1
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-03-07 10:47:21 +0100

    add admi view

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TyskaForSmaUpptackare.Data;
+
+namespace TyskaForSmaUpptackare.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Orders()
+        {
+            var orders = _context.Orders
+                .Include(o => o.User) // Ladda relaterad användardata
+                .Include(o => o.OrderItems) // Ladda relaterade orderartiklar
+                .ThenInclude(oi => oi.Product) // Ladda relaterad produktdata
+                .OrderByDescending(o => o.CreatedAt) // Sortera efter senaste order
+                .ToList();
+
+            return View("orders", orders); // Skicka datan till vyn
+        }
+    }
+}
