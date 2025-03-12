18caa25f27a423bf4e4eb6c9aa3a1bbb16953066
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-03-12 12:14:33 +0100

    add product content to front page

---
+++ ViewModel/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using TyskaForSmaUpptackare.Models;
+
+namespace TyskaForSmaUpptackare.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
